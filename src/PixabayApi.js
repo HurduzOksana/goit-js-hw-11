9408fc0cfd367aa9a42a7fddba53d1d6379d5ac4
Hurduz Oksana <hurduzoksana@gmail.com>
2022-12-03 01:50:26 +0200

    adding styles

--- src/PixabayApi.js
+++ src/PixabayApi.js
@@ -1,37 +1,66 @@
 
-// Описуємо клас для роботи з API
+import axios from 'axios';
+
+const BASE_URL = `https://pixabay.com/api/`;
+
 export class PixabayApi {
-     BASE_URL = `https://pixabay.com/api/`;
-     API_KEY = `31697968-406cab2af0ae45e7393df2600`;
+  BASE_URL = `https://pixabay.com/api/`;
+  API_KEY = `31697968-406cab2af0ae45e7393df2600`;
     
   constructor() {
     this.page = 1;
     this.query = null;
     this.per_page = 40;
-    }
+  }
       
-      fetchPhotosByQuery() {
-    const searchParams = new URLSearchParams({
-      q: this.query,
-      page: this.page,
-      per_page: this.per_page,
-      key: this.API_KEY,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: 'true',
+  fetchPhotosByQuery() {
+    return axios.get(`${this.BASE_URL}?${searchParams}?${this.API_KEY}`, {
+      params: {
+        q: this.query,
+        page: this.page,
+        per_page: this.per_page,
+        key: this.API_KEY,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+      },
     });
-      
-        return fetch(
-            `${this.BASE_URL}?${searchParams}?${this.API_KEY}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(response.status);
-                }
-                return response.json();
-            })
-    
-    }
+  }
 }
+
+// // Описуємо клас для роботи з API
+// export class PixabayApi {
+//      BASE_URL = `https://pixabay.com/api/`;
+//      API_KEY = `31697968-406cab2af0ae45e7393df2600`;
+    
+//   constructor() {
+//     this.page = 1;
+//     this.query = null;
+//     this.per_page = 40;
+//     }
+      
+//       fetchPhotosByQuery() {
+//     const searchParams = new URLSearchParams({
+//       q: this.query,
+//       page: this.page,
+//       per_page: this.per_page,
+//       key: this.API_KEY,
+//       image_type: 'photo',
+//       orientation: 'horizontal',
+//       safesearch: 'true',
+//     });
+      
+//         return fetch(
+//             `${this.BASE_URL}?${searchParams}?${this.API_KEY}`)
+//             .then(response => {
+//                 if (!response.ok) {
+//                     throw new Error(response.status);
+//                 }
+//                 return response.json();
+//             })
+    
+//     }
+// }
 
 
 
--- src/index.js
+++ src/index.js
@@ -4,55 +4,61 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 
-
 const searchFormElem = document.querySelector(`.search-form`);
-// const formInput = document.querySelector(`input`);
 const loadMoreBtn = document.querySelector(`.load-more`);
 const picturesGallery = document.querySelector(`.gallery`);
 
- // Створюємо екземпляр від класу
 const pixabayApiExemp = new PixabayApi();
 
-// Функція пошуку картинки
+// ================================================================================
 function onSearch(event) {
     event.preventDefault();
     pixabayApiExemp.page = 1;
-    
-    // Дістаємо значення інпуту
     pixabayApiExemp.query = event.currentTarget.elements.query.value;
-    // console.log(pixabayApiExemp)
-
+   
     pixabayApiExemp.fetchPhotosByQuery()   
         .then((data) => {
+            if (data.total === 0) {
+                picturesGallery.innerHTML = '';
+                loadMoreBtn.classList.add(`is-hidden`)
+                onFetchError();
+                event.target.reset();
+                return;
+            }
+           
             console.log(data)
 
             renderPicturesGallery(data.hits);
-            new SimpleLightbox('.gallery a').refresh();
-
+            
+            simpleLightbox.refresh();
+            
             onFetchMessage(data.totalHits);
+
             loadMoreBtn.classList.remove(`is-hidden`)
-           
+        
         })
-        .catch(onFetchError)
+         .catch(err => {
+      console.log(err);
+    });
 }
 
-// // Функція відмалювання в HTML списку картинок
+
 function renderPicturesGallery(data) {
     const markup = data
     .map((data) => {
-        return `<div class="photo-card">
+        return `
+        <div class="photo-card">
         <a href="${data.largeImageURL}"><img class="images" src="${data.webformatURL}" alt="${data.tags}" loading="lazy"/></a>
-  <div class="info">
-    <p class="info-item"><b>Likes</b>${data.likes}</p>
-    <p class="info-item"><b>Views</b>${data.views}</p>
-    <p class="info-item"><b>Comments</b>${data.comments}</p>
-    <p class="info-item"><b>Downloads</b>${data.downloads}</p>
-  </div>
-</div>`
+        <div class="info">
+        <p class="info-item"><b>Likes</b>${data.likes}</p>
+        <p class="info-item"><b>Views</b>${data.views}</p>
+        <p class="info-item"><b>Comments</b>${data.comments}</p>
+        <p class="info-item"><b>Downloads</b>${data.downloads}</p>
+        </div>
+        </div>`
     })
     .join("");
     picturesGallery.innerHTML = markup;
-    
 }
 
 function onClickLoad(event) {
@@ -61,18 +67,22 @@ function onClickLoad(event) {
     pixabayApiExemp.fetchPhotosByQuery()
         .then(data => {
             
-            if (pixabayApiExemp.page === data.totalHits) {
+            if (pixabayApiExemp.page > data.totalHits) {
                 loadMoreBtn.classList.add(`is-hidden`)
-                
                 onFetchInfo();
+                return;
             }
             
-            picturesGallery.insertAdjacentHTML(`beforeend`, renderPicturesGallery(data.hits));
+        
+            renderPicturesGallery(data.hits);
+            
+            simpleLightbox.refresh();
+            scroll();
             
         })
         .catch(err => {
-        
-    })
+      console.log(err);
+    });
 }
 
 function onFetchError(error) {
@@ -87,7 +97,22 @@ function onFetchMessage(totalHits) {
     Notify.success(`Hooray! We found ${totalHits} images.`);
 }
 
-// Вішаємо слухача подію сабміту 
+
+const simpleLightbox = new SimpleLightbox('.gallery a', { 
+  captionsData: `alt`,
+  captionDelay: `250`
+ });
+
+function scroll() {
+    const { height: cardHeight } =
+    picturesGallery.firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
+ }
+
 searchFormElem.addEventListener(`submit`, onSearch);
 loadMoreBtn.addEventListener(`click`, onClickLoad)
 
